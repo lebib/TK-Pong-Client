af630e69415343da3a290d0d843ccecb674aa94e
DocWinter <sauze.thomas@gmail.com>
2015-05-14 23:53:47 +0200

    Do some test

--- client/game.js
+++ client/game.js
@@ -24,8 +24,8 @@ $(function()
     paddle_p2.onload = setAssetReady;
     ball.onload = setAssetReady;
 
-    paddle_p1.src = 'img/paddle_p1.png';
-    paddle_p2.src = 'img/paddle_p2.png';
+    paddle_p1.src = 'img/p1.png';
+    paddle_p2.src = 'img/p2.png';
     ball.src = 'img/ball.png';
 
     function setAssetReady() {
@@ -33,7 +33,7 @@ $(function()
     }
 
     ctx.fillRect(0, 0, 720, 480);
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = "grey";
     ctx.fillText("Loading", 10, 10);
     var preloader = setInterval(preloading, 33);
     var gameloop;
@@ -45,37 +45,42 @@ $(function()
         }
     }
 
-    var paddle_p1_x = 0,
+    var paddle_p1_x = 720/2 - 100/2,
         paddle_p1_y = 0,
-        paddle_p2_x = 0,
-        paddle_p2_y = 0,
+        paddle_p2_x = 720/2 - 100/2,
+        paddle_p2_y = 450,
         ball_x = 0,
         ball_y = 0;
 
     function update()
     {
-        var socket = new WebSocket("ws://localhost")
-        socket.onopen = function() {
-            document.addEventListener('keydown', function(event) {
-                if(event.keyCode == 37) {
-                    socket.send('MOVE_LEFT');
-                } else if(event.keyCode == 39) {
-                    socket.send('MOVE_RIGHT');
-                }
-            });
-        }
+    //     var socket = new WebSocket("ws://localhost")
+    //     socket.onopen = function() {
+    //         document.addEventListener('keydown', function(event) {
+    //             if(event.keyCode == 37) {
+    //                 socket.send('MOVE_LEFT');
+    //             } else if(event.keyCode == 39) {
+    //                 socket.send('MOVE_RIGHT');
+    //             }
+    //         });
+    //     }
 
-        socket.onmessage = function(evt) {
-            var received_msg = evt.data;
-            // handle the parsing of the json.
-        }
+    //     socket.onmessage = function(evt) {
+    //         var received_msg = evt.data;
+    //         // handle the parsing of the json.
+    //     }
 
-        socket.onclose = function(event) {
-            context.fillStyle = "grey";
-            context.fillRect(0, 0, 720, 480);
-            context.drawImage(paddle_p1, paddle_p1_x, paddle_p1_px);
-            context.drawImage(paddle_p2, paddle_p2_x, paddle_p2_y);
-            context.drawImage(ball, ball_x, ball_y);
-        }
+    //     socket.onclose = function(event) {
+    //         ctx.fillStyle = "grey";
+    //         ctx.fillRect(0, 0, 720, 480);
+    //         ctx.drawImage(paddle_p1, paddle_p1_x, paddle_p1_px);
+    //         ctx.drawImage(paddle_p2, paddle_p2_x, paddle_p2_y);
+    //         ctx.drawImage(ball, ball_x, ball_y);
+    //     }
+        ctx.fillStyle = "grey";
+        ctx.fillRect(0, 0, 720, 480);
+        ctx.drawImage(paddle_p1, paddle_p1_x, paddle_p1_y);
+        ctx.drawImage(paddle_p2, paddle_p2_x, paddle_p2_y);
+        ctx.drawImage(ball, ball_x, ball_y);
     }
 })
